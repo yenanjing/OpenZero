fba9a59fb1e12a910c8b6748fa8fd3cedeb547b8
wenweiwwli <wenweiwwli@tencent.com>
2025-03-03 19:40:15 +0800

    chore: fix metric error

--- scripts/train_open_industry_1node_resume_bf16.sh
+++ scripts/train_open_industry_1node_resume_bf16.sh
@@ -22,12 +22,15 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=$MODEL_PATH\
     actor_rollout_ref.actor.optim.lr=3e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.ppo_micro_batch_size=2 \
     actor_rollout_ref.actor.ulysses_sequence_parallel_size=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    +actor_rollout_ref.actor.dtype=bf16 \
+    +actor_rollout_ref.actor.fsdp_config.dtype=bf16 \
+    actor_rollout_ref.rollout.dtype=bf16 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.fsdp_config.grad_offload=True \
